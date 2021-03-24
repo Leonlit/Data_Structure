d6d629d4d0cd71c786bf16bae99fc27a59d064d3
Leon <leonlit123@gmail.com>
2021-03-24 13:14:02 +0800

    text now can be printed one by one

--- NameInCharArray.java
+++ NameInCharArray.java
@@ -3,24 +3,32 @@ import java.util.Scanner;
 class NameInCharArray {
     public static void main (String[] args) {
         Scanner input = new Scanner (System.in);
-        System.out.println("\nWelcome user, please input your name and I'll print it out again:");
+        printStringWithDelays("\nWelcome user, please input your name and I'll print it out again:");
         String name = input.nextLine();
         input.close();
         char arr[] = name.toCharArray();
 
-        System.out.println("Re-printing your name, give me a moment");
-        System.out.print("\nOh so you're ");
-        for (char character: arr) {
+        printStringWithDelays("Re-printing your name, give me a moment");
+        printStringWithDelays("\nOh so you're ");
+        printWithDelays(arr);
+
+        printStringWithDelays("\nNice to meet you.");
+        printStringWithDelays("\nOk, that's all bye....");
+        System.out.println();
+    }
+
+    static void printStringWithDelays (String text) {
+        printWithDelays(text.toCharArray());
+    }
+
+    static void printWithDelays (char characters[]) {
+        for (char alphabets: characters) {
             try {
-                Thread.sleep(100);
-                System.out.print(character);
+                Thread.sleep(50);
+                System.out.print(alphabets);
             }catch (InterruptedException ex) {
                 System.out.println(ex);
             }
         }
-
-        System.out.println("\nNice to meet you.");
-        System.out.println("\nOk, that's all bye....");
-        System.out.println();
-    }   
+    }
 }
